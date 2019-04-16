c2dcc74f347bd93fd5f7df3223d088ea20fcc730
Alpesh Valaki <alpesh@entigrity.com>
2019-04-16 15:42:22 +0530

    [IMP] working hour selection

--- indimedi_crm/models/project_task.py
+++ indimedi_crm/models/project_task.py
@@ -17,7 +17,7 @@ class Project(models.Model):
     timesheet_phone = fields.Char('Employee Phone', track_visibility='onchange')
     invoicing_type_id = fields.Many2one('job.invoicing', string="Invoicing Type", track_visibility='onchange')
     invoice_start_date = fields.Date(string="Billing Start Date", track_visibility='onchange')
-    hour_selection = fields.Selection([('10','10 Hours'),('20','20 Hours'),('30','30 Hours'),
+    hour_selection = fields.Selection([('10','10 Hours'),('15', '15'),('20','20 Hours'),('25', '25'),('30','30 Hours'),
                                        ('40','40 Hours'),('80','80 Hours'),('90','90 Hours'),
                                        ('100','100 Hours'),('40_20','40-20 Hours'),
                                        ('20_10','20-10 Hours'),('160','160 Hours'),
