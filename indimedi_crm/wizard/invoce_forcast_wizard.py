67a25c143f24895839e9aa7869706767e2017dff
Alpesh Valaki <alpesh@entigrity.com>
2019-04-29 19:42:59 +0530

    [ADD] forecast report changes

--- indimedi_crm/wizard/invoce_forcast_wizard.py
+++ indimedi_crm/wizard/invoce_forcast_wizard.py
@@ -30,10 +30,9 @@ class InvoiceForecastWizard(models.Model):
         
         project_id = self.env['project.project'].search([('id','=',332)])
         
-        print"project===========",project_id.name
+        print"project===========",project_id.name, project_id.date_of_join
         
         #get start and end Date
-        today = datetime.now().date()
         
         start_date = datetime.strptime(self.start_date, DEFAULT_SERVER_DATE_FORMAT)
         end_date = start_date + relativedelta(months=+self.forecast_month)
@@ -45,32 +44,26 @@ class InvoiceForecastWizard(models.Model):
         #get billing info
             
         #mapp with billing history
+        date_of_join = project_id.date_of_join
+        print"date_of_join===============",end_date
         
-        while(start_date <= end_date):
+        domain = [
+                ('invoice_start_date', '>=', end_date),
+                ('invoice_start_date', '!=', False),
+                ('invoice_end_date', '!=', False),
+                ('project_id', '=', project_id.id),
+                ]
+        
+        bill = self.env['billing.history'].search(domain)
+        current_rate = 0
+        hour_selection = 0
+        if bill:
+            current_rate = bill[-1:].rate_per_hour
+            hour_selection = bill[-1:].hour_selection
+        else:
+            current_rate = project_id.rate_per_hour
+            hour_selection = project_id.hour_selection
+        
+        print"hour_selection=======",hour_selection,current_rate
             
-            date = start_date
-            
-            
-            domain_bill = [
-                    ('invoice_end_date', '>=', end_date),
-                    ('invoice_start_date', '!=', False),
-                    ('invoice_end_date', '!=', False),
-                    ('project_id', '=', project_id.id),
-                    ]
-            
-            bill = self.env['billing.history'].sudo().search(domain_bill, limit=1)
-            if bill:
-                hour_selection = bill.hour_selection
-                rate = project_id.rate_per_hour
-            else:
-                if project_id.date_of_joining >= start_date:
-                    hour_selection = project_id.hour_selection
-                    rate = project_id.rate_per_hour
-                    
-            print"bill========",start_date, hour_selection, rate
-            
-            
-            print"dates=====================",start_date, end_date
-            
-            start_date = start_date + timedelta(days=1)        
         raise UserError("1")
